d5bc3a224850a9b6298b04e53c1260f72d23aca8
Danilo Santos <116808289+DSantos29@users.noreply.github.com>
2023-04-24 05:57:27 -0300

    Add files via upload

---
+++ frontend/src/pages/IPerfil.jsx
@@ -0,0 +1,15 @@
+import Painel from "../assets/components/Painel/Login";
+import Navbar from "../assets/components/Navbar/Navbar"
+import Footer from "../assets/components/Footer/Footer"
+
+function IPerfil () {
+    return(
+        <div>
+        <Navbar />
+        <Painel/>
+        </div>
+    )
+    
+}
+
+export default IPerfil
---
+++ frontend/src/pages/Sobre.jsx
@@ -0,0 +1,21 @@
+import Banner from "../assets/components/Banner/Banner"
+import Footer from "../assets/components/Footer/Footer"
+import Historia from "../assets/components/Historia/Historia"
+import Missao from "../assets/components/Missao/Missao"
+import Navbar from "../assets/components/Navbar/Navbar"
+
+function Sobre () {
+    return(
+        <div>
+            <Navbar />
+            <Banner />
+            <Historia />
+            <Missao />
+            <Footer />
+
+        </div>
+    )
+    
+}
+
+export default Sobre
